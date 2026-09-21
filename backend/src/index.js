d96267ac55b94c1f79e7567e6d2f444f0c590709
sharonpr1998 <sharonpr1998@gmail.com>
2026-09-21 20:51:44 +0530

    initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,10 @@
+import "dotenv/config"
+import express from "express"
+
+const app = express()
+const PORT=process.env.PORT
+
+
+app.listen(PORT,()=>{
+  console.log("Server running on port :",PORT)
+})
