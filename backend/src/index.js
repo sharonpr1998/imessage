76ecaa689dcd4ec07afe5ceac71e97b03619efcc
sharonpr1998 <sharonpr1998@gmail.com>
2026-09-22 10:35:21 +0530

    Add static file serving and build script to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,10 +3,15 @@ import express from "express"
 import { connectDB } from "./config/db.js"
 import { clerkMiddleware } from '@clerk/express'
 import cors from "cors"
+import fs from "fs"
+import path from "path"
+
 
 const app = express()
 const PORT=process.env.PORT
 const FRONTEND_URL= process.env.FRONTEND_URL
+
+const publicDir = path.join(process.cwd(), 'public');
 
 app.use(express.json())
 app.use(cors({origin:FRONTEND_URL,credentials:true}))
@@ -16,6 +21,13 @@ app.get("/health",(req,res)=>{
   res.status(200).json({ok:true})
 })
 
+if(fs.existsSync(publicDir)){
+  app.use(express.static(publicDir));
+
+  app.get("/{*any}",(req,res,next)=>{
+    res.sendFile(path.join(publicDir, 'index.html'),(err)=>next(err));
+  })
+}
 
 app.listen(PORT,()=>{
   connectDB();
